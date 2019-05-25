2d291d692dca85fc278f700a87d7b9c87b5fd1a0
sirm2z <sirm2zisv@gmail.com>
2019-05-25 16:35:50 +0800

    Added JwtStrategy

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,41 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { UserService } from '../shared/user.service';
 import { LoginDTO, RegisterDTO } from './auth.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private authService: AuthService,
+  ) {}
+
+  @Get()
+  @UseGuards(AuthGuard('jwt'))
+  tempAuth() {
+    return { auth: 'works' };
+  }
 
   @Post('login')
-  login(@Body() userDto: LoginDTO) {
-    return this.userService.findByLogin(userDto);
+  async login(@Body() userDto: LoginDTO) {
+    const user = await this.userService.findByLogin(userDto);
+    const payload = {
+      username: user.username,
+      seller: user.seller,
+    };
+    const token = await this.authService.signPayload(payload);
+    return { user, token };
   }
 
   @Post('register')
-  register(@Body() userDto: RegisterDTO) {
-    return this.userService.create(userDto);
+  async register(@Body() userDto: RegisterDTO) {
+    const user = await this.userService.create(userDto);
+    const payload = {
+      username: user.username,
+      seller: user.seller,
+    };
+    const token = await this.authService.signPayload(payload);
+    return { user, token };
   }
 }
